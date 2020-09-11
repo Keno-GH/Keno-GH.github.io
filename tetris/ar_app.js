5796f3af54dbd4d5d04121b9626aa8aa05aa5d70
Keno <neocv9@gmail.com>
2020-09-11 10:38:36 -0300

    Added manual

--- tetris/ar_app.js
+++ tetris/ar_app.js
@@ -4,7 +4,7 @@ var scene, camera, renderer, clock, deltaTime, totalTime;
 
 var arToolkitSource, arToolkitContext;
 
-var mesh1, mesh2, meshImagen, meshImagen2, meshImagen3;
+var mesh1, mesh2, meshImagen0, meshImagen, meshImagen2, meshImagen3, meshImagen4, meshImagen5, meshImagen6;
 
 var markerManual1;
 
@@ -107,54 +107,107 @@ function init() {
     //Marker setup
     /////////////////////////////////////////////////
 
-    markerManual1 = new THREE.Group(); //creamos un grupo de objetos
-    scene.add(markerManual1); // agregamos el grupo a la escena. 
+    markerPag0 = new THREE.Group(); //creamos un grupo de objetos
+    scene.add(markerPag0); // agregamos el grupo a la escena. 
 
     //Creamos nuestro marcador 
-    let markerControl = new THREEx.ArMarkerControls(arToolkitContext, markerManual1, {
-
-        type: 'pattern', patternUrl: 'data/AR-Pattern.patt',
+    let markerControl0 = new THREEx.ArMarkerControls(arToolkitContext, markerPag0, {
+        type: 'pattern', patternUrl: 'data/pattern-patronpag0.patt',
     });
 
-    markerPag1 = new THREE.Group(); //creamos un grupo de objetos
-    scene.add(markerPag1); // agregamos el grupo a la escena. 
+    markerPag1 = new THREE.Group();
+    scene.add(markerPag1);
 
-    //Creamos nuestro marcador 
-    let markerControl2 = new THREEx.ArMarkerControls(arToolkitContext, markerPag1, {
+    let markerControl = new THREEx.ArMarkerControls(arToolkitContext, markerPag1, {
+        type: 'pattern', patternUrl: 'data/patten-patronpag1.patt',
+    });
 
-        type: 'pattern', patternUrl: 'data/pattern-patronpag1.patt',
+    markerPag2 = new THREE.Group();
+    scene.add(markerPag2); 
+
+    let markerControl2 = new THREEx.ArMarkerControls(arToolkitContext, markerPag2, {
+        type: 'pattern', patternUrl: 'data/pattern-patronpag2.patt',
+    });
+
+    markerPag3 = new THREE.Group();
+    scene.add(markerPag3);
+
+    let markerControl3 = new THREEx.ArMarkerControls(arToolkitContext, markerPag3, {
+        type: 'pattern', patternUrl: 'data/pattern-patronpag3.patt',
+    });
+
+    markerPag4 = new THREE.Group();
+    scene.add(markerPag4);
+
+    let markerControl4 = new THREEx.ArMarkerControls(arToolkitContext, markerPag4, {
+        type: 'pattern', patternUrl: 'data/pattern-patronpag4.patt',
+    });
+
+    markerPag5 = new THREE.Group();
+    scene.add(markerPag5);
+
+    let markerControl5 = new THREEx.ArMarkerControls(arToolkitContext, markerPag5, {
+        type: 'pattern', patternUrl: 'data/pattern-patronpag5.patt',
     });
 
     ////////////////////////////////////////////////
     //Creacion de geometrias - Imagenes
     ///////////////////////////////////////////////
 
-    /* let geo1 = new THREE.PlaneBufferGeometry(2,1.5,4,4);
-        let loader2 = new THREE.TextureLoader();
-        let textura3 = loader2.load('images/poster.jpg'); //Esta es la imagen que agregamos
-        let material1 = new THREE.MeshBasicMaterial({map:textura3});
+    let geo0 = new THREE.PlaneBufferGeometry(2,1,4,4);
+        let loader0 = new THREE.TextureLoader();
+        let textura0 = loader0.load('images/pag0.png');
+        let material0 = new THREE.MeshBasicMaterial({map:textura0});
 
-        let meshImagen = new THREE.Mesh(geo1,material1);
-        meshImagen.rotation.x = -Math.PI/2;
-        markerManual1.add(meshImagen); */
+        let meshImagen0 = new THREE.Mesh(geo0,material0);
+        meshImagen0.rotation.x = -Math.PI/2;
+        markerPag0.add(meshImagen0);
 
-    let geo1 = new THREE.PlaneBufferGeometry(2,2,4,4);
+    let geo1 = new THREE.PlaneBufferGeometry(2,1,4,4);
         let loader1 = new THREE.TextureLoader();
-        let textura1 = loader1.load('images/texto1.png');
+        let textura1 = loader1.load('images/pag1.png');
         let material1 = new THREE.MeshBasicMaterial({map:textura1});
 
         let meshImagen = new THREE.Mesh(geo1,material1);
-        markerManual1.add(meshImagen);
+        meshImagen.rotation.x = -Math.PI/2;
+        markerPag1.add(meshImagen);
 
-    let geo2 = new THREE.PlaneBufferGeometry(2,1.5,4,4);
+    let geo2 = new THREE.PlaneBufferGeometry(2,1,4,4);
         let loader2 = new THREE.TextureLoader();
-        let textura2 = loader2.load('images/pag1.png'); //Esta es la imagen que agregamos
+        let textura2 = loader2.load('images/pag2.png');
         let material2 = new THREE.MeshBasicMaterial({map:textura2});
 
-        let meshImagen2 = new THREE.Mesh(geo1,material2);
+        let meshImagen2 = new THREE.Mesh(geo2,material2);
         meshImagen2.rotation.x = -Math.PI/2;
-        markerPag1.add(meshImagen2);
+        markerPag2.add(meshImagen2);
+
+    let geo3 = new THREE.PlaneBufferGeometry(2,1,4,4);
+        let loader3 = new THREE.TextureLoader();
+        let textura3 = loader3.load('images/pag3.png');
+        let material3 = new THREE.MeshBasicMaterial({map:textura3});
+
+        let meshImagen3 = new THREE.Mesh(geo3,material3);
+        meshImagen3.rotation.x = -Math.PI/2;
+        markerPag3.add(meshImagen3);
     
+    let geo4 = new THREE.PlaneBufferGeometry(2,1,4,4);
+        let loader4 = new THREE.TextureLoader();
+        let textura4 = loader4.load('images/pag4.png');
+        let material4 = new THREE.MeshBasicMaterial({map:textura4});
+
+        let meshImagen4 = new THREE.Mesh(geo4,material4);
+        meshImagen4.rotation.x = -Math.PI/2;
+        markerPag4.add(meshImagen4);
+
+    let geo5 = new THREE.PlaneBufferGeometry(2,1,4,4);
+        let loader5 = new THREE.TextureLoader();
+        let textura5 = loader5.load('images/pag5.png');
+        let material5 = new THREE.MeshBasicMaterial({map:textura5});
+
+        let meshImagen5 = new THREE.Mesh(geo5,material5);
+        meshImagen5.rotation.x = -Math.PI/2;
+        markerPag5.add(meshImagen5);
+
 }
 
 function update() {
@@ -162,6 +215,7 @@ function update() {
     if (arToolkitSource.ready !== false) {
         arToolkitContext.update(arToolkitSource.domElement);
     }
+    
 }
 
 function render() {
